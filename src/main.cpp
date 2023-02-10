5d366037bd7e7290191b6f36472546ad38840955
pattarawan21 <nadekopapama@gmail.com>
2023-02-10 14:00:30 +0700

    powerbank update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,55 @@
 #include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Bounce2.h>
+#define green1 33
+#define ldr1 26
+#define ldr2 27
+//#define lazer1 25
+//#define lazer2 14
+#define red1 32
+#define buzzer1 18
+
+//#define buzzer 8 
+//void get
+// void PW1(){
+//   //รับ powerbank status ตัวแปรสักอัน
+//   //if #borrow
+//   if (analogRead(ldr1)<4095){
+//     analogWrite(green1,1);
+//     //post avaliable
+//   }
+//   else {
+//     analogWrite(green1,0);
+//     analogWrite(red1,1);
+//     //post not avaliable
+//   }
+//   //else #get not borrow
+//   if (analogRead(ldr1)==4095){
+//     digitalWrite(buzzer1, HIGH);
+//     //post not avalible
+//   }
+// }
+int val;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(green1, OUTPUT);
+  digitalWrite(green1, 1);
+  pinMode(green1, OUTPUT);
+  digitalWrite(red1, 1);
+  pinMode(red1, OUTPUT);
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  Serial.begin(115200);
+  pinMode(buzzer1,OUTPUT);
+  //red1.setColor(0,1);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Serial.println(analogRead(ldr2));
+  // delay(100);
+  // //touch_2();
+
 }
