25062b4d4364a2da9eb624224a313744ed5295a0
pattarawan21 <nadekopapama@gmail.com>
2023-02-10 15:40:09 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,34 @@
 #define ldr2 27
 #define red1 32
 #define buzzer1 18
-
+#define Borrow_mai 0
 
 void pw1(){
   if (Borrow_mai){
       digitalWrite(green1,1);
-      if (analogRead(ldr1)==4095){
+      if (analogRead(ldr1)>=3900){
         digitalWrite(green1,0);
         digitalWrite(red1,1);
       }
+      else  {
+        digitalWrite(green1,1);
+        digitalWrite(red1,0);
+      }
   }
   else{
-    if(analogRead(ldr1)==4095){
+   
+    if(analogRead(ldr1)>=3900){
       digitalWrite(red1,1);
       digitalWrite(buzzer1, HIGH);
       pai_laew(PB_ID);
     }
-
+    else{
+      digitalWrite(buzzer1, LOW);
+      digitalWrite(red1,0);
+    }
+  
   }
-  }
+}
 
 void kuen(){
 if(analogRead(ldr1)<4095){
@@ -39,9 +48,9 @@ if(analogRead(ldr1)<4095){
 void setup() {
   Serial.begin(9600);
   pinMode(green1, OUTPUT);
-  digitalWrite(green1, 1);
+  digitalWrite(green1, 0);
   pinMode(green1, OUTPUT);
-  digitalWrite(red1, 1);
+  digitalWrite(red1, 0);
   pinMode(red1, OUTPUT);
   pinMode(red1, OUTPUT);
   pinMode(green1, OUTPUT);
@@ -51,5 +60,7 @@ void setup() {
 }
 
 void loop() {
-
+pw1();
+Serial.println(analogRead(ldr1));
+kuen();
 }
